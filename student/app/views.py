d06ba21421a2ff38e8f0327617a37dac9566adda
mxtadithya <adiadithyaajayan@gmail.com>
2026-04-13 09:11:41 +0530

    django

---
+++ student/app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('home page')
+
+
+def students(request):
+    return HttpResponse('students')
+
+def about(request):
+    return render(request,'about.html')
